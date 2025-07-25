43b1fd5a3afdead0790dcca71b6ee17e99f200b1
reino08 <170168346+reino08@users.noreply.github.com>
2025-07-24 22:37:56 -0400

    add error

--- src/types/errors/chat.rs
+++ src/types/errors/chat.rs
@@ -6,4 +6,16 @@
 )]
 pub enum ChatError {
     CommandError { message: String },
+    Error { error_type: ChatErrorType },
+}
+
+#[derive(Debug, serde::Deserialize)]
+#[serde(
+    tag = "type",
+    rename_all = "camelCase",
+    rename_all_fields = "camelCase"
+)]
+pub enum ChatErrorType {
+    CommandError { message: String },
+    ContactNotFound { contact_name: String },
 }
